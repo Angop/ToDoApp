f323adb11892d4ac23cdd54abed4ecf5f48d3af5
joshuavd <joshuavd@att.net>
2021-02-12 14:25:27 -0800

    Create mongo_database.py

---
+++ Backend/mongo_database.py
@@ -0,0 +1,49 @@
+import pymongo
+from bson import ObjectId
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+class User(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)  #change if your db is in another host and port
+    collection = db_client["users"]["users_list"]  #db name is 'users' and collection name is 'users_list'
+
+    def find_all(self):
+        users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name(self, name):
+        users = list(self.collection.find({"name": name}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
