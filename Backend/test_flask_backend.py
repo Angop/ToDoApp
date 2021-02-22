4b52d7c4ee44865861b0fe3b227eb237aa26aa2a
Akerlin <56051184+Angop@users.noreply.github.com>
2021-02-22 15:09:55 -0800

    Update test_flask_backend.py

--- Backend/test_flask_backend.py
+++ Backend/test_flask_backend.py
@@ -1,20 +1,6 @@
 import pytest
 import flask_backend
 
-def test_find_users_by_name_success():  
-    expected = { 
-        'users_list' :
-        [
-            {
-                'id' : 'abc123',            
-                'name': 'Mac',
-                'job': 'Bouncer',
-            },
-            {
-                'id' : 'ppp222',            
-                'name': 'Mac',
-                'job': 'Professor',
-            },        
-        ]
-    }  
-    assert flask_backend.find_users_by_name("Mac") == expected
+def hello_world_success():  
+    expected = flask_backend.hello_world()
+    assert "Hello, World!" == expected
