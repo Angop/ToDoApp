b01b7a23f902a0aadb9344e44a312ecd8e8565ec
Akerlin <56051184+Angop@users.noreply.github.com>
2021-03-08 13:58:53 -0800

    added update task testcase

--- Backend/test_mongo.py
+++ Backend/test_mongo.py
@@ -2,6 +2,7 @@ import pytest
 import mongo_database
 from pymongo import MongoClient
 import json
+from bson import ObjectId
 
 
 @pytest.fixture
@@ -14,7 +15,7 @@ def clear_db():
 def test_find_all_empty(clear_db):
     assert mongo_database.User().find_all() == []
 
-def test_save(clear_db):
+def test_save_new_task(clear_db):
     given_task = json.dumps({'task': 'task1', 'desc': 'desc1', 'priority': '10', 'checked': True, 'type': 'Other', 'date': '2021-03-18'})
     newUser = mongo_database.User(json.loads(given_task))
     newUser.save()
@@ -26,6 +27,23 @@ def test_save(clear_db):
     assert tasks[0]['checked']
     assert tasks[0]['type'] == 'Other'
     assert tasks[0]['date'] == '2021-03-18'
+
+def test_save_update_task(clear_db):
+    #TODO
+    given_task = json.dumps({'task': 'task1', 'desc': 'desc1', 'priority': '10', 'checked': True, 'type': 'Other', 'date': '2021-03-18'})
+    newUser = mongo_database.User(json.loads(given_task))
+    newUser.save()
+    orig_tasks = mongo_database.User().find_all()
+    print(orig_tasks)
+
+    update_task = json.dumps({'_id': orig_tasks[0]['_id'], 'task': 'UPDATED', 'desc': 'desc1', 'priority': '10', 'checked': True, 'type': 'Other', 'date': '2021-03-18'})
+    updatedUser = mongo_database.User(json.loads(update_task))
+    updatedUser.save()
+    tasks = mongo_database.User().find_all()
+    print(tasks)
+    assert len(tasks) == 1
+    assert tasks[0]['task'] == 'UPDATED'
+
     
 def test_remove(clear_db):
     given_task = json.dumps({'task': 'task1', 'desc': 'desc1', 'priority': '10', 'checked': True, 'type': 'Other', 'date': '2021-03-18'})
